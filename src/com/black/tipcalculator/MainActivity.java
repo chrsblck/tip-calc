fceb180a00b6f044d48a86c4169260178f91e8d4
Chris Black <chrisbblack@gmail.com>
2013-12-31 17:48:51 -0800

    adding feature - per person

--- src/com/black/tipcalculator/MainActivity.java
+++ src/com/black/tipcalculator/MainActivity.java
@@ -11,8 +11,10 @@ public class MainActivity extends Activity {
 	public static final double fifteenPercent = .15;
 	public static final double eighteenPercent = .18;
 	public static final double twentyPercent = .2;
+	public double lastCalculated;
 	public double totalTip;
 	public double totalAmount;
+	public double totalPerPerson;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,21 +31,75 @@ public class MainActivity extends Activity {
     }
     
     public void calculateFifteenPercent(View view) {
-    	this.caclulateTip(fifteenPercent);
+    	lastCalculated = fifteenPercent;
+    	calculateTip(fifteenPercent);
     }
     
     public void calculateEighteenPercent(View view) {
-    	this.caclulateTip(eighteenPercent);
+    	lastCalculated = eighteenPercent;
+    	calculateTip(eighteenPercent);
     }
     
     public void calculateTwentyPercent(View view) {
-    	this.caclulateTip(twentyPercent);
+    	lastCalculated = twentyPercent;
+    	calculateTip(twentyPercent);
     }
     
-    public void caclulateTip(double percentage) {
+    public int getPeopleInParty() {
+    	int number;
+        TextView people = (TextView)findViewById(R.id.personsPerParty);
+
+        String tmpNum = people.getText().toString();
+        number = Integer.parseInt(tmpNum);
+        
+        if(number < 1) {
+        	number = 1;
+        }
+        
+        return number;
+    }
+    
+    public void decrementPeopleInParty(View view) {
+    	int personsToDisplay;
+    	TextView people = (TextView)findViewById(R.id.personsPerParty);
+       	
+    	try {
+       		personsToDisplay = getPeopleInParty() - 1;
+       		if (personsToDisplay < 1) {
+       			personsToDisplay = 1;
+       		}
+        	people.setText(Integer.toString(personsToDisplay));
+        	
+        	if (lastCalculated > 0) {
+        		calculateTip(lastCalculated);
+        	}
+       	} catch (Exception e) {
+       		System.out.println(e.toString());
+       	}
+    }
+    
+    public void addPeopleInParty(View view) {
+    	int personsToDisplay;
+       	TextView people = (TextView)findViewById(R.id.personsPerParty);
+       	
+       	try {
+       		personsToDisplay = getPeopleInParty() + 1;
+        	people.setText(Integer.toString(personsToDisplay));
+        	
+        	if (lastCalculated > 0) {
+        		calculateTip(lastCalculated);
+        	}
+       	} catch (Exception e) {
+       		System.out.println(e.toString());
+       	}
+    }
+    
+    public void calculateTip(double percentage) {
         EditText editAmount = (EditText)findViewById(R.id.billAmountText);
         TextView tipText = (TextView)findViewById(R.id.tipTotalView);
         TextView totalText = (TextView)findViewById(R.id.totalView);
+        TextView perPerson = (TextView)findViewById(R.id.totalPerPersonView);
+        
         float number;
         
         if (!editTextIsEmpty(editAmount)) {
@@ -54,12 +110,16 @@ public class MainActivity extends Activity {
         
     	totalTip = number * percentage;
     	totalAmount = totalTip + number;
+    	totalPerPerson = totalAmount / getPeopleInParty();
     	
     	String niceTotalTip = (String.format(getString(R.string.amount_format), totalTip));
     	String niceTotal    = (String.format(getString(R.string.amount_format), totalAmount));
+    	String niceTotalPerPerson = (String.format(getString(R.string.amount_format), totalPerPerson));
 
+    	
     	tipText.setText(getString(R.string.tip_label) + niceTotalTip);
     	totalText.setText(getString(R.string.total_label) + niceTotal);
+    	perPerson.setText(getString(R.string.per_person_total) + niceTotalPerPerson);
     }
     
     private boolean editTextIsEmpty(EditText edit) {
